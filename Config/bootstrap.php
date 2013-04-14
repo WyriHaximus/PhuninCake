4ff31a361acb01aa4cb7596ad91893362ee3f93a
Cees-Jan <Cees-Jan@xps8500.home.wyrihaxim.us>
2013-04-14 18:35:06 +0200

    Added a stop command

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -1,19 +1,11 @@
 <?php
 
-/*
- * This file is part of PhuninCake.
- *
- ** (c) 2013 Cees-Jan Kiewiet
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 Configure::write('PhuninCake', array(
     'Node' => array(
         'connection' => array(
             'address' => '0.0.0.0',
-            'port' => 12345,
+            'port' => 14001,
         ),
+        'killConnection' => 'tcp://127.0.0.1:15001',
     ),
 ));
--- Console/Command/NodeShell.php
+++ Console/Command/NodeShell.php
@@ -1,16 +1,8 @@
 <?php
 
-/*
- * This file is part of PhuninCake.
- *
- ** (c) 2013 Cees-Jan Kiewiet
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 App::uses('CakeEvent', 'Event');
 App::uses('CakeEventManager', 'Event');
+App::uses('Security', 'Utility');
 
 use React\EventLoop\Factory as LoopFactory;
 
@@ -19,25 +11,54 @@ class NodeShell extends Shell {
     private $loop;
     private $node;
     
-    public function __construct($stdout = null, $stderr = null, $stdin = null) {
-        parent::__construct($stdout, $stderr, $stdin);
-        
+    public function run() {
         $this->loop = LoopFactory::create();
         
+        $this->runSetupNode();
+        
+        CakeEventManager::instance()->dispatch(new CakeEvent('PhuninCake.Node.run', $this, array(
+            'loop' => $this->loop,
+            'node' => $this->node,
+        )));
+        
+        $this->runSetupKillSwitch();
+        
+        $this->loop->run();
+    }
+    
+    private function runSetupNode() {
         $this->node = new \PhuninNode\Node($this->loop, Configure::read('PhuninCake.Node.connection.port'), Configure::read('PhuninCake.Node.connection.address'), false);
         $this->node->addPlugin(new \PhuninNode\Plugins\Plugins());
         $this->node->addPlugin(new \PhuninNode\Plugins\PluginsCategories());
         $this->node->addPlugin(new \PhuninNode\Plugins\MemoryUsage());
         $this->node->addPlugin(new \PhuninNode\Plugins\Uptime());
-        
-        CakeEventManager::instance()->dispatch(new CakeEvent('PhuninCake.Node.construct', $this, array(
-            'loop' => $this->loop,
-            'node' => $this->node,
-        )));
     }
     
-    public function run() {
-        $this->loop->run();
+    private function runSetupKillSwitch() {
+        $loop = $this->loop;
+        $context = new React\ZMQ\Context($this->loop);
+        $socket = $context->getSocket(ZMQ::SOCKET_PULL);
+        $socket->bind(Configure::read('PhuninCake.Node.killConnection'));
+        $socket->on('message', function($command) use ($loop) {
+            if (Security::hash($command, 'sha256', true) == Security::hash($this->getKillCommand(), 'sha256', true)) {
+                $loop->stop();
+            }
+        });
+    }
+    
+    public function stop() {
+        $zmq = new ZMQContext(1);
+        $nodeConnection = $zmq->getSocket(ZMQ::SOCKET_PUSH, 'xyz');
+        $nodeConnection->connect(Configure::read('PhuninCake.Node.killConnection'));
+        $nodeConnection->send($this->getKillCommand());
+    }
+    
+    private function getKillCommand() {
+        $killCommand = array(
+            'configHash' => Security::hash(serialize(Configure::read('PhuninCake.Node')), 'sha256', true),
+        );
+        
+        return serialize($killCommand);
     }
     
 }
